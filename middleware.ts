eeaec2c41022537717836928d934138a081e84f0
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-10-20 21:52:45 +0800

    protect routes

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+export { default } from "next-auth/middleware";
+// import { NextRequest, NextResponse } from "next/server";
+// import middleware from "next-auth/middleware";
+// export function middleware(request: NextRequest) {
+//     return NextResponse.redirect(new URL("/new-user",request.url))
+// }
+
+// export default middleware;
+
+
+export const config = {
+    // *: zero or more: /users/:id*
+    // +: one ore more
+    // ?: zero or one
+    matcher: ['/users/:id*']
+}
