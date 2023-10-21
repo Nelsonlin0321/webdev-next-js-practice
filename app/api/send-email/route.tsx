61a968f8248997280d34f00fef87341f7b97b0cd
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-10-21 18:06:14 +0800

    add registered domain

--- app/api/send-email/route.tsx
+++ app/api/send-email/route.tsx
@@ -1,12 +1,16 @@
 import { Resend } from "resend";
 import WelcomeTemplate from "@/emails/WelcomeTemplate";
+import { NextRequest, NextResponse } from "next/server";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-export async function POST() {
+export async function POST(request: NextRequest) {
+  const body = await request.json();
   resend.emails.send({
-    from: "...",
-    to: "someone@outlook.com",
-    subject: "...",
-    react: <WelcomeTemplate name="someone" />,
+    from: "Acme <onboarding@nelsonds.com>",
+    to: "nelsonlin0321@gmail.com",
+    subject: "Testing",
+    react: <WelcomeTemplate name={body.name!} />,
   });
+
+  return NextResponse.json({});
 }
